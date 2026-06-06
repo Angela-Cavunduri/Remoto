49d01442f90253cd5fbdc2c8bd97d036b4172333
Ângela Cavunduri <angelacavundurijulcira@gmail.com>
2026-06-06 21:08:28 +0100

    Permitir que o cors aceite pedido do pc da Dejanir

--- app/main.py
+++ app/main.py
@@ -39,7 +39,15 @@ app = FastAPI(
 # Configuração de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://localhost:8080",
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:8080",
+    ], 
+    allow_origin_regex="https://.*", # Permite qualquer domínio em Produção (Vercel, Netlify, Render)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
