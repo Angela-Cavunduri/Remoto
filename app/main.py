ed8078f99e099693ad65bf576acd509006298bf2
Ângela Cavunduri <angelacavundurijulcira@gmail.com>
2026-06-29 14:46:54 +0100

    o cors, de novo

--- app/main.py
+++ app/main.py
@@ -67,7 +67,6 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:8080",
     ], 
-    allow_origin_regex="https://.*", # Permite qualquer domínio em Produção (Vercel, Netlify, Render)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
